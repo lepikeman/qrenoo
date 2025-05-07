1211ef9c3c07914395f7666f0ad2c1f06e94426d
lepikeman <63370994+lepikeman@users.noreply.github.com>
2025-05-07 22:35:21 +0200

    add page jobs

---
+++ app/(jobs)/tatoueur/page.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const metadata = {
+  title:
+    "Logiciel de rendez-vous pour coiffeurs et salons de coiffure | Qrenoo",
+  description:
+    "Solution de prise de rendez-vous en ligne spécialement conçue pour les coiffeurs et salons de coiffure. Gérez votre planning et réduisez les annulations.",
+};
+
+export default function TatoueurSolution() {
+  return (
+    <main>
+      <h1>Solution de prise de rendez-vous pour coiffeurs</h1>
+      <p>
+        Spécialement conçue pour répondre aux besoins des salons de coiffure et
+        barbiers
+      </p>
+
+      {/* Contenu spécifique aux coiffeurs */}
+    </main>
+  );
+}
